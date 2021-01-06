07f6e59b93f29e70d60caf3411d1a0a28eea7f03
artxe <balgoon24@gmail.com>
2021-01-06 20:52:44 +0900

    asd

--- erbsdc/db/weapon/WhipDB.js
+++ erbsdc/db/weapon/WhipDB.js
@@ -2,7 +2,7 @@ const Whip = [
     {
          Type: "Whip"
         ,Name: "Whip"
-        ,Attack_Power: 9
+        ,Attack_Power: 12
         ,Attack_Speed: 0
         ,Critical_Strike_Chance: 0
         ,Critical_Strike_Damage: 0
@@ -26,20 +26,20 @@ const Whip = [
         ,Out_of_Combat_Movement_Speed: 0
         ,Vision_Range: 0
         ,Attack_Range: 0
-        ,Base_Range: 0
-        ,Atk_Speed: 0
-        ,Title: "Whip\n\nAttack Power +9"
+        ,Base_Range: 2.35
+        ,Atk_Speed: 0.55
+        ,Title: "Whip\n\nAttack Power +12"
         ,Rarity: "Common"
     }
     ,{
          Type: "Whip"
         ,Name: "Rope_Cuffs"
-        ,Attack_Power: 19
+        ,Attack_Power: 24
         ,Attack_Speed: 0
         ,Critical_Strike_Chance: 0
         ,Critical_Strike_Damage: 0
         ,Life_Steal: 0
-        ,Extra_Normal_Attack_Damage: 0
+        ,Extra_Normal_Attack_Damage: 6
         ,Extra_Normal_Attack_Damage_Percent: 0
         ,Skill_Amplification: 0
         ,Skill_Amplification_Percent: 0
@@ -58,15 +58,15 @@ const Whip = [
         ,Out_of_Combat_Movement_Speed: 0
         ,Vision_Range: 0
         ,Attack_Range: 0
-        ,Base_Range: 0
-        ,Atk_Speed: 0
-        ,Title: "Rope Cuffs\n\nAttack Power +19"
+        ,Base_Range: 2.35
+        ,Atk_Speed: 0.55
+        ,Title: "Rope Cuffs\n\nAttack Power +24\nSkill Amplification +6"
         ,Rarity: "Uncommon"
     }
     ,{
          Type: "Whip"
         ,Name: "Bullwhip"
-        ,Attack_Power: 19
+        ,Attack_Power: 30
         ,Attack_Speed: 0
         ,Critical_Strike_Chance: 0
         ,Critical_Strike_Damage: 0
@@ -90,15 +90,15 @@ const Whip = [
         ,Out_of_Combat_Movement_Speed: 0
         ,Vision_Range: 0
         ,Attack_Range: 0
-        ,Base_Range: 0
-        ,Atk_Speed: 0
-        ,Title: "Bullwhip\n\nAttack Power +19"
+        ,Base_Range: 2.35
+        ,Atk_Speed: 0.55
+        ,Title: "Bullwhip\n\nAttack Power +30"
         ,Rarity: "Uncommon"
     }
     ,{
          Type: "Whip"
         ,Name: "Wind_Whip"
-        ,Attack_Power: 25
+        ,Attack_Power: 24
         ,Attack_Speed: 0
         ,Critical_Strike_Chance: 0
         ,Critical_Strike_Damage: 0
@@ -122,15 +122,15 @@ const Whip = [
         ,Out_of_Combat_Movement_Speed: 0
         ,Vision_Range: 0
         ,Attack_Range: 0
-        ,Base_Range: 0
-        ,Atk_Speed: 0
-        ,Title: "Wind Whip\n\nAttack Power +25\nSkill Amplification +20"
+        ,Base_Range: 2.35
+        ,Atk_Speed: 0.55
+        ,Title: "Wind Whip\n\nAttack Power +24\nSkill Amplification +20"
         ,Rarity: "Rare"
     }
     ,{
          Type: "Whip"
         ,Name: "Thunder_Whip"
-        ,Attack_Power: 19
+        ,Attack_Power: 30
         ,Attack_Speed: 0
         ,Critical_Strike_Chance: 0
         ,Critical_Strike_Damage: 0
@@ -138,7 +138,7 @@ const Whip = [
         ,Extra_Normal_Attack_Damage: 0
         ,Extra_Normal_Attack_Damage_Percent: 0
         ,Skill_Amplification: 0
-        ,Skill_Amplification_Percent: 0
+        ,Skill_Amplification_Percent: 20
         ,Cooldown_Reduction: 0
         ,SP_Regen: 0
         ,SP_Regen_Percent: 0
@@ -154,15 +154,15 @@ const Whip = [
         ,Out_of_Combat_Movement_Speed: 0
         ,Vision_Range: 0
         ,Attack_Range: 0
-        ,Base_Range: 0
-        ,Atk_Speed: 0
-        ,Title: "Thunder Whip\n\nAttack Power +55\nSkill Amplification +18%"
+        ,Base_Range: 2.35
+        ,Atk_Speed: 0.55
+        ,Title: "Thunder Whip\n\nAttack Power +30\nSkill Amplification +20%"
         ,Rarity: "Rare"
     }
     ,{
          Type: "Whip"
         ,Name: "Lightning_Whip"
-        ,Attack_Power: 41
+        ,Attack_Power: 42
         ,Attack_Speed: 20
         ,Critical_Strike_Chance: 0
         ,Critical_Strike_Damage: 0
@@ -186,14 +186,46 @@ const Whip = [
         ,Out_of_Combat_Movement_Speed: 0
         ,Vision_Range: 0
         ,Attack_Range: 0
-        ,Base_Range: 0
-        ,Atk_Speed: 0
-        ,Title: "Lightning Whip\n\nAttack Power +41\nAttack Speed +20%"
+        ,Base_Range: 2.35
+        ,Atk_Speed: 0.55
+        ,Title: "Lightning Whip\n\nAttack Power +42\nAttack Speed +20%"
         ,Rarity: "Rare"
     }
     ,{
          Type: "Whip"
         ,Name: "Gleipnir"
+        ,Attack_Power: 58
+        ,Attack_Speed: 35
+        ,Critical_Strike_Chance: 0
+        ,Critical_Strike_Damage: 0
+        ,Life_Steal: 0
+        ,Extra_Normal_Attack_Damage: 0
+        ,Extra_Normal_Attack_Damage_Percent: 0
+        ,Skill_Amplification: 0
+        ,Skill_Amplification_Percent: 0
+        ,Cooldown_Reduction: 0
+        ,SP_Regen: 0
+        ,SP_Regen_Percent: 0
+        ,Skill_Damage_Reduction: 0
+        ,Skill_Damage_Reduction_Percent: 0
+        ,Defense: 0
+        ,Max_HP: 0
+        ,Max_SP: 0
+        ,HP_Regen: 0
+        ,HP_Regen_Percent: 0
+        ,Normal_Attack_Damage_Reduction: 0
+        ,Move_Speed: 0
+        ,Out_of_Combat_Movement_Speed: 0
+        ,Vision_Range: 0
+        ,Attack_Range: 0
+        ,Base_Range: 2.35
+        ,Atk_Speed: 0.55
+        ,Title: "Gleipnir\n\nAttack Power +58\nAttack Speed +35%"
+        ,Rarity: "Epic"
+    }
+    ,{
+         Type: "Whip"
+        ,Name: "Plasma_Whip"
         ,Attack_Power: 54
         ,Attack_Speed: 30
         ,Critical_Strike_Chance: 0
@@ -216,49 +248,17 @@ const Whip = [
         ,Normal_Attack_Damage_Reduction: 0
         ,Move_Speed: 0
         ,Out_of_Combat_Movement_Speed: 0
-        ,Vision_Range: 0
-        ,Attack_Range: 0
-        ,Base_Range: 0
-        ,Atk_Speed: 0
-        ,Title: "Gleipnir\n\nAttack Power +54\nAttack Speed +30%"
-        ,Rarity: "Epic"
-    }
-    ,{
-         Type: "Whip"
-        ,Name: "Plasma_Whip"
-        ,Attack_Power: 49
-        ,Attack_Speed: 20
-        ,Critical_Strike_Chance: 0
-        ,Critical_Strike_Damage: 0
-        ,Life_Steal: 0
-        ,Extra_Normal_Attack_Damage: 0
-        ,Extra_Normal_Attack_Damage_Percent: 0
-        ,Skill_Amplification: 0
-        ,Skill_Amplification_Percent: 0
-        ,Cooldown_Reduction: 0
-        ,SP_Regen: 0
-        ,SP_Regen_Percent: 0
-        ,Skill_Damage_Reduction: 0
-        ,Skill_Damage_Reduction_Percent: 0
-        ,Defense: 0
-        ,Max_HP: 0
-        ,Max_SP: 0
-        ,HP_Regen: 0
-        ,HP_Regen_Percent: 0
-        ,Normal_Attack_Damage_Reduction: 0
-        ,Move_Speed: 0
-        ,Out_of_Combat_Movement_Speed: 0
         ,Vision_Range: 2.3
         ,Attack_Range: 0
-        ,Base_Range: 0
-        ,Atk_Speed: 0
-        ,Title: "Plasma Whip\n\nAttack Power +49\nAttack Speed +20%\nVision Range +2.3"
+        ,Base_Range: 2.35
+        ,Atk_Speed: 0.55
+        ,Title: "Plasma Whip\n\nAttack Power +54\nAttack Speed +30%\nVision Range +2.3"
         ,Rarity: "Epic"
     }
     ,{
          Type: "Whip"
         ,Name: "Whip_of_Nine_Bloody_Tails"
-        ,Attack_Power: 70
+        ,Attack_Power: 65
         ,Attack_Speed: 0
         ,Critical_Strike_Chance: 0
         ,Critical_Strike_Damage: 0
@@ -282,9 +282,9 @@ const Whip = [
         ,Out_of_Combat_Movement_Speed: 0
         ,Vision_Range: 0
         ,Attack_Range: 0
-        ,Base_Range: 0
-        ,Atk_Speed: 0
-        ,Title: "Whip of Nine Bloody Tails\n\nAttack Power +70\nLife Steal +20%\nSkill Amplification +40"
+        ,Base_Range: 2.35
+        ,Atk_Speed: 0.55
+        ,Title: "Whip of Nine Bloody Tails\n\nAttack Power +65\nLife Steal +20%\nSkill Amplification +40"
         ,Rarity: "Legendary"
     }
 ];
