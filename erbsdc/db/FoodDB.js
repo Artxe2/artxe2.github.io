095c2cd223024c3eaa9fc272b6af904412950a57
artxe <balgoon24@gmail.com>
2021-01-09 00:20:53 +0900

    asd

--- erbsdc/db/FoodDB.js
+++ erbsdc/db/FoodDB.js
@@ -36,6 +36,48 @@ const Food = [
     }
     ,{
         Type: "Food"
+        ,Name: "Mocha_Bread_xiu"
+        ,HP_Regen: 910
+        ,Title: "Mocha Bread xiu\n\nHP Regen +910"
+        ,Rarity: "Rare"
+    }
+    ,{
+        Type: "Food"
+        ,Name: "Choco_Pie_Box"
+        ,HP_Regen: 733
+        ,Title: "Choco Pie Box\n\nHP Regen +733"
+        ,Rarity: "Rare"
+    }
+    ,{
+        Type: "Food"
+        ,Name: "Choco_Pie_Box_xiu"
+        ,HP_Regen: 953
+        ,Title: "Choco Pie Box xiu\n\nHP Regen +953"
+        ,Rarity: "Rare"
+    }
+    ,{
+        Type: "Food"
+        ,Name: "Cold_Noodles"
+        ,HP_Regen: 650
+        ,Title: "Cold Noodles\n\nHP Regen +650"
+        ,Rarity: "Rare"
+    }
+    ,{
+        Type: "Food"
+        ,Name: "Spicy_Fish_Stew"
+        ,HP_Regen: 550
+        ,Title: "Spicy Fish Stew\n\nHP Regen +550"
+        ,Rarity: "Rare"
+    }
+    ,{
+        Type: "Food"
+        ,Name: "Spicy_Fish_Stew_xiu"
+        ,HP_Regen: 715
+        ,Title: "Spicy Fish Stew xiu\n\nHP Regen +715"
+        ,Rarity: "Rare"
+    }
+    ,{
+        Type: "Food"
         ,Name: "Fish_And_Chips"
         ,HP_Regen: 900
         ,Title: "Fish And Chips\n\nHP Regen +900"
