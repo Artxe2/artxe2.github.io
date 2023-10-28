b22a230df3c29cb0b97f7817dbc11c7409b8aa4d
artxe2 <balgoon24@gmail.com>
2023-10-28 18:57:25 +0900

    20231028

--- README.md
+++ README.md
@@ -3,12 +3,3 @@
 ## Don't reinvent the wheel
 > ### **[/css-lube](https://artxe2.github.io/css-lube)**: CSS Lube
 > Highly-optimized JIT CSS Generator
-
-> ### **[/async-lube](https://www.npmjs.com/package/async-lube)**: Async Lube
-> Simplify Asynchronous Operations in JavaScript
-
-> ### **[/date-lube](https://www.npmjs.com/package/date-lube)**: Date Lube
-> Simplify Date Manipulation in JavaScript
-
-> ### **[/fetch-lube](https://www.npmjs.com/package/fetch-lube)**: Fetch Lube
-> Simplify Fetch API Usage in JavaScript
--- test/test.js
+++ test/test.js
@@ -7,7 +7,7 @@ const h6 = 16 ** 6
 const chars = '0123456789abcdef'
 const background = []
 
-for (let i = 0; i < h6; i += 1000) {
+for (let i = 0; i < h6; i += 3000) {
 	background.push(
 		`${chars[Math.trunc(i / h5)]
 		}${chars[Math.trunc(i % h5 / h4)]
@@ -20,32 +20,44 @@ for (let i = 0; i < h6; i += 1000) {
 var fs = require('fs')
 
 fs.writeFile(
-	'inline-style.html'
-	, `<html><head><title>Inline Style Performance Test</title></head><body>${background.map(v=>`<div style="background:#${v};"><span style="color:#${v};filter:invert(1);">${v}</span></div>`).join("")}</body></html>`
-	,'utf8'
-	, function(error){console.log('inline-style.html end')}
-)
-fs.writeFile(
-	'css-lube.html'
-	, `<html><head><title>CSS Lube Performance Test</title><script src="https://artxe2.github.io/css-lube/css-lube.min.js"></script></head><body>${background.map(v=>`<div class="bg=#${v}"><span class="c=#${v} ft=invert(1)">${v}</span></div>`).join("")}</body></html>`
-	,'utf8'
-	, function(error){console.log('css-lube.html end')}
-)
-fs.writeFile(
-	'tailwind-jit.html'
-	, `<html><head><title>Tailwind JIT Performance Test</title><script src="https://unpkg.com/tailwindcss-jit-cdn"></script></head><body>${background.map(v=>`<div class="bg-[#${v}]"><span class="c-[#${v}] invert">${v}</span></div>`).join("")}</body></html>`
-	,'utf8'
-	, function(error){console.log('tailwind-jit.html end')}
-)
-fs.writeFile(
-	'atomic-css.html'
-	, `<html><head><title>Atomic CSS Performance Test</title><link rel="stylesheet" href="./atomic-css.css"/></head><body>${background.map(v=>`<div class="Bg(#${v})"><span class="C(#${v}) Ft(invert(1))">${v}</span></div>`).join("")}</body></html>`
-	,'utf8'
-	, function(error){console.log('atomic-css.html end')}
-)
-fs.writeFile(
 	'atomic-css.css'
 	, `.Ft\\(invert\\(1\\)\\){filter:invert(1)}${background.map(v=>`.Bg\\(\\#${v}\\){background:#${v}}.C\\(\\#${v}\\){color:#${v}}`).join("")}`
 	,'utf8'
 	, function(error){console.log('atomic-css.css end')}
+)
+fs.writeFile(
+	'atomic-css.html'
+	, `<!DOCTYPE html><html><head><title>Atomic CSS Performance Test</title><link rel="stylesheet" href="./atomic-css.css"/></head><body>${background.map(v=>`<div class="Bg(#${v})"><span class="C(#${v}) Ft(invert(1))">${v}</span></div>`).join("")}</body></html>`
+	,'utf8'
+	, function(error){console.log('atomic-css.html end')}
+)
+fs.writeFile(
+	'css-lube.html'
+	, `<!DOCTYPE html><html><head><title>CSS Lube Performance Test</title><script src="https://artxe2.github.io/css-lube/css-lube.min.js"></script></head><body>${background.map(v=>`<div class="bg=#${v}"><span class="c=#${v} ft=invert(1)">${v}</span></div>`).join("")}</body></html>`
+	,'utf8'
+	, function(error){console.log('css-lube.html end')}
+)
+fs.writeFile(
+	'css-lube-test.html'
+	, `<!DOCTYPE html><html><head><title>CSS Lube Performance Test</title><script src="../../css-lube/packages/lib/css-lube.js"></script></head><body>${background.map(v=>`<div class="bg=#${v}"><span class="c=#${v} ft=invert(1)">${v}</span></div>`).join("")}</body></html>`
+	,'utf8'
+	, function(error){console.log('css-lube.html end')}
+)
+fs.writeFile(
+	'inline-style.html'
+	, `<!DOCTYPE html><html><head><title>Inline Style Performance Test</title></head><body>${background.map(v=>`<div style="background:#${v};"><span style="color:#${v};filter:invert(1);">${v}</span></div>`).join("")}</body></html>`
+	,'utf8'
+	, function(error){console.log('inline-style.html end')}
+)
+fs.writeFile(
+	'master-css.html'
+	, `<!DOCTYPE html><html><head><title>Master CSS Performance Test</title><script src="https://cdn.master.co/css"></script></head><body>${background.map(v=>`<div class="bg:#${v}"><span class="text-fill:#${v} filter:invert(1)">${v}</span></div>`).join("")}</body></html>`
+	,'utf8'
+	, function(error){console.log('css-lube.html end')}
+)
+fs.writeFile(
+	'tailwind-cdn.html'
+	, `<!DOCTYPE html><html><head><title>Tailwind JIT Performance Test</title><script src="https://cdn.tailwindcss.com"></script></head><body>${background.map(v=>`<div class="bg-[#${v}]"><span class="text-[color:#${v}] invert">${v}</span></div>`).join("")}</body></html>`
+	,'utf8'
+	, function(error){console.log('tailwind-cdn.html end')}
 )
